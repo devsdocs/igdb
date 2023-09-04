b4cb3a8b5f8222d63c4b5c349985a45ce9cf870a
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-09-05 06:59:01 +0900

    Update example

--- example/igdb_example.dart
+++ example/igdb_example.dart
@@ -1,6 +1,28 @@
-// import 'package:igdb/igdb.dart';
+import 'dart:io';
 
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+import 'package:igdb/igdb.dart';
+
+void main() async {
+  final clientId = Platform.environment['TWITCH_CLIENT'];
+  final clientSecret = Platform.environment['TWITCH_SECRET'];
+  final client = IGDBClient(clientId!, clientSecret!);
+  const igdbRequestParameters = IGDBRequestParameters(
+    search: 'CS:GO',
+  );
+  final json = await client.gameJson(
+    igdbRequestParameters,
+  );
+  final countJson = await client.gameCountJson(
+    igdbRequestParameters,
+  );
+  final proto = await client.gameProto(
+    igdbRequestParameters,
+  );
+  final countProto = await client.gameCountProto(
+    igdbRequestParameters,
+  );
+  print(json);
+  print(countJson);
+  print(proto?.toProto3Json());
+  print(countProto?.toProto3Json());
+}
